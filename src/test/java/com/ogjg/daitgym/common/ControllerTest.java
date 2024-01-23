2dfd324656e6830425d53113e957717358089c52
DongJinLee <dlehdwls21@naver.com>
2024-01-23 20:00:24 +0900

    Test : Follow Rest Doc을 통해 문서화
    ### FollowController Spring Rest Doc을 통해 문서화

--- src/test/java/com/ogjg/daitgym/common/ControllerTest.java
+++ src/test/java/com/ogjg/daitgym/common/ControllerTest.java
@@ -2,16 +2,18 @@ package com.ogjg.daitgym.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ogjg.daitgym.alarm.service.FCMInitializer;
-import com.ogjg.daitgym.exercise.service.ExerciseHelper;
-import com.ogjg.daitgym.exercise.service.ExerciseService;
+import com.ogjg.daitgym.config.security.details.OAuth2JwtUserDetails;
+import com.ogjg.daitgym.config.security.jwt.dto.JwtUserClaimsDto;
+import com.ogjg.daitgym.domain.Role;
 import com.ogjg.daitgym.feed.service.FeedExerciseJournalService;
 import com.ogjg.daitgym.feed.service.FeedJournalHelper;
 import com.ogjg.daitgym.feed.service.UserFeedExerciseJournalService;
-import com.ogjg.daitgym.follow.service.FollowService;
 import com.ogjg.daitgym.journal.service.ExerciseJournalHelper;
 import com.ogjg.daitgym.journal.service.ExerciseJournalService;
 import com.ogjg.daitgym.user.controller.LoginController;
 import com.ogjg.daitgym.user.service.AuthService;
+import com.ogjg.daitgym.user.service.UserHelper;
+import com.ogjg.daitgym.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +23,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
-@AutoConfigureRestDocs
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class ControllerTest {
 
     @RegisterExtension
@@ -40,6 +45,19 @@ public class ControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(documentationConfiguration(restDocumentation))
                 .build();
+
+        // JwtUserDetails 객체 생성
+        OAuth2JwtUserDetails userDetails = new OAuth2JwtUserDetails(
+                JwtUserClaimsDto.builder()
+                        .email("ogjg@mail.com")
+                        .nickname("ogjg")
+                        .role(Role.ADMIN)
+                        .build()
+        );
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+        SecurityContextHolder.setContext(securityContext);
     }
 
     @Autowired
@@ -54,8 +72,6 @@ public class ControllerTest {
     @MockBean
     protected ExerciseJournalService exerciseJournalService;
 
-    @MockBean
-    protected FollowService followService;
 
     @MockBean
     protected FeedExerciseJournalService feedExerciseJournalService;
@@ -67,10 +83,10 @@ public class ControllerTest {
     protected UserFeedExerciseJournalService userFeedExerciseJournalService;
 
     @MockBean
-    protected ExerciseHelper exerciseHelper;
+    protected UserHelper userHelper;
 
     @MockBean
-    protected ExerciseService exerciseService;
+    protected UserService userService;
 
     @MockBean
     protected FCMInitializer fcmInitializer;
--- src/test/java/com/ogjg/daitgym/follow/controller/FollowControllerTest.java
+++ src/test/java/com/ogjg/daitgym/follow/controller/FollowControllerTest.java
@@ -1,7 +1,185 @@
 package com.ogjg.daitgym.follow.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.ogjg.daitgym.common.ControllerTest;
+import com.ogjg.daitgym.follow.dto.response.FollowCountResponse;
+import com.ogjg.daitgym.follow.dto.response.FollowListDto;
+import com.ogjg.daitgym.follow.dto.response.FollowListResponse;
+import com.ogjg.daitgym.follow.repository.FollowRepository;
+import com.ogjg.daitgym.follow.service.FollowService;
+import com.ogjg.daitgym.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-class FollowControllerTest {
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@Slf4j
+class FollowControllerTest extends ControllerTest {
+
+    @MockBean
+    private FollowRepository followRepository;
+
+    @MockBean
+    private FollowService followService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("팔로우")
+    public void follow() throws Exception {
+        mockMvc.perform(post("/api/follows/{nickname}", "targetNickname"))
+                .andExpect(jsonPath("$.status.code").value("200"))
+                .andExpect(jsonPath("$.status.message").value("OK"))
+                .andDo(document("follow",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("nickname").description("팔로우 당하는 사람의 닉네임")
+                        ),
+                        responseFields(
+                                fieldWithPath("status.code").description("응답 코드"),
+                                fieldWithPath("status.message").description("응답 메시지"),
+                                fieldWithPath("data").description("응답 데이터 현재는 null")
+                        )))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("언팔로우")
+    public void unfollow() throws Exception {
+        mockMvc.perform(delete("/api/follows/{nickname}", "targetNickname"))
+                .andExpect(jsonPath("$.status.code").value("200"))
+                .andExpect(jsonPath("$.status.message").value("OK"))
+                .andDo(document("unfollow",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("nickname").description("팔로우 취소 당하는 사람의 닉네임")
+                        ),
+                        responseFields(
+                                fieldWithPath("status.code").description("응답 코드"),
+                                fieldWithPath("status.message").description("응답 메시지"),
+                                fieldWithPath("data").description("응답 데이터 현재는 null")
+                        )))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("닉네임 유저가 팔로우중인 유저 수")
+    public void followingCounts() throws Exception {
+        when(followService.followingCount(Mockito.anyString())).thenReturn(new FollowCountResponse(1));
+
+        mockMvc.perform(get("/api/follows/following-counts/{nickname}", "targetNickname"))
+                .andExpect(jsonPath("$.status.code").value("200"))
+                .andExpect(jsonPath("$.status.message").value("OK"))
+                .andDo(document("followingCounts",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("nickname").description("대상이 되는 유저 닉네임")
+                        ),
+                        responseFields(
+                                fieldWithPath("status.code").description("응답 코드"),
+                                fieldWithPath("status.message").description("응답 메시지"),
+                                fieldWithPath("data.followCounts").description("닉네임 사용자가 팔로우한 유저 수")
+                        )
+                )).andDo(print());
+    }
+
+    @Test
+    @DisplayName("닉네임 유저를 팔로우한 유저 수")
+    public void followerCounts() throws Exception {
+        when(followService.followerCount(Mockito.anyString())).thenReturn(new FollowCountResponse(1));
+
+        mockMvc.perform(get("/api/follows/follower-counts/{nickname}", "targetNickname"))
+                .andExpect(jsonPath("$.status.code").value("200"))
+                .andExpect(jsonPath("$.status.message").value("OK"))
+                .andDo(document("followerCounts",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("nickname").description("대상이 되는 유저 닉네임")
+                        ),
+                        responseFields(
+                                fieldWithPath("status.code").description("응답 코드"),
+                                fieldWithPath("status.message").description("응답 메시지"),
+                                fieldWithPath("data.followCounts").description("닉네임 사용자가 팔로우한 유저 수")
+                        )
+                )).andDo(print());
+
+    }
+
+
+    @Test
+    @DisplayName("닉네임 유저를 팔로우한 친구들 목록 가져오기")
+    public void followerList() throws Exception {
+        List<FollowListDto> followListDto = List.of(
+                new FollowListDto("둘리1.jpg", "둘리1", "저는 둘리1입니다"),
+                new FollowListDto("둘리2.jpg", "둘리2", "저는 둘리2입니다")
+        );
+        when(followService.followerList(anyString())).thenReturn(new FollowListResponse(followListDto));
+
+        mockMvc.perform(get("/api/follows/follower-list/{nickname}", "targetNickname"))
+                .andExpect(jsonPath("$.status.code").value("200"))
+                .andExpect(jsonPath("$.status.message").value("OK"))
+                .andDo(document("follower-list",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("nickname").description("대상이 되는 유저 닉네임")
+                        ),
+                        responseFields(
+                                fieldWithPath("status.code").description("응답 코드"),
+                                fieldWithPath("status.message").description("응답 메시지"),
+                                fieldWithPath("data.followList[].imageUrl").description("팔로워 이미지"),
+                                fieldWithPath("data.followList[].nickname").description("팔로워 닉네임"),
+                                fieldWithPath("data.followList[].intro").description("팔로워 소개"),
+                                fieldWithPath("data.followList[].score").description("팔로워 인바디점수 인바디 점수가 없으면 0점으로 표시")
+                        )
+                )).andDo(print());
+    }
+
+    @Test
+    @DisplayName("닉네임 유저가 팔로우한 친구들 목록 가져오기")
+    public void followingList() throws Exception {
+        List<FollowListDto> followListDto = List.of(
+                new FollowListDto("둘리1.jpg", "둘리1", "저는 둘리1입니다"),
+                new FollowListDto("둘리2.jpg", "둘리2", "저는 둘리2입니다")
+        );
+        when(followService.followingList(anyString())).thenReturn(new FollowListResponse(followListDto));
+
+        mockMvc.perform(get("/api/follows/following-list/{nickname}", "targetNickname"))
+                .andExpect(jsonPath("$.status.code").value("200"))
+                .andExpect(jsonPath("$.status.message").value("OK"))
+                .andDo(document("following-list",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("nickname").description("대상이 되는 유저 닉네임")
+                        ),
+                        responseFields(
+                                fieldWithPath("status.code").description("응답 코드"),
+                                fieldWithPath("status.message").description("응답 메시지"),
+                                fieldWithPath("data.followList[].imageUrl").description("팔로워 이미지"),
+                                fieldWithPath("data.followList[].nickname").description("팔로워 닉네임"),
+                                fieldWithPath("data.followList[].intro").description("팔로워 소개"),
+                                fieldWithPath("data.followList[].score").description("팔로워 인바디점수 인바디 점수가 없으면 0점으로 표시")
+                        )
+                )).andDo(print());
+    }
 }
